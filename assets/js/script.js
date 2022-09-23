61cd41ef87e5ea5eb4ca346a56cba48b2de0ff3e
TheDKnight <113733655@umail.ucc.ie>
2022-09-23 20:45:12 +0000

    added base functions

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+}
+
+
+
+function userChoice(){
+
+}
+
+function computerChoice(){
+
+}
+
+function userScore() {
+   
+}
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function computerScore() {
+  
+}
