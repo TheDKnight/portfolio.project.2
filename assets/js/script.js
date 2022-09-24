b83d8b2043d0c40c97bf60b3527db07af1fbe567
TheDKnight <113733655@umail.ucc.ie>
2022-09-24 22:55:22 +0000

    got the player and computer choice to read in the winner choose function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,52 @@
 
-
-
-
-//function userChoice(){
     document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
      button.addEventListener("click", function(event){
-     let players = event.target.id 
-    console.log(players);
+     userChoice(event)
+     chooseWinner()
   })
+  }
   
+});
+
+function userChoice(){
+  
+  let  players = event.target.id  
+//console.log(players)
+  return players 
+   //
+   
+ // console.log(players)
+   // return userChoice(event)
+  
+   //return userChoice(players)
+   
 }
-}
-r//eturn userChoice()
-//}
+
 
 function computerChoice(){
-
     let hands = ["rock","paper","scissor","lizard","spock"]
     let compChoice = Math.floor(Math.random()*hands.length);
     return hands[compChoice];
 
 }
+
+
+function chooseWinner(){
+ playerSelction = userChoice()
+ compSelection = computerChoice()
+
+if (playerSelction === compSelection) {
+  console.log("Draw")
+  document.getElementById('score').innerHTML = `Draw: You Picked ${playerSelction} Computer Picked ${compSelection} ` 
+}//else if 
+
+console.log(playerSelction)
+console.log(compSelection)
+};
+
 
 /**
  * Gets the users winning score
